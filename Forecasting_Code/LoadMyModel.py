be55e3485cb8499d4cf3c4d5cddc6400dad5e56b
LGsus20 <105143494+LGsus20@users.noreply.github.com>
2024-04-24 16:09:56 -0700

    Fixed some bugs

--- Forecasting_Code/LoadMyModel.py
+++ Forecasting_Code/LoadMyModel.py
@@ -1,15 +1,14 @@
 from statsforecast import StatsForecast
 from statsforecast.models import AutoARIMA, AutoETS, AutoTheta, AutoCES
-import pandas as pd
-import numpy as np
 
 PATH = r"C:\Users\Jesus\Downloads\forecasting\Weather-Forecasting-Scraping\Forecasting_Code\StatsForecast_2023-2024.pkl"
+# PATH = the route of your model/s, should end in .pkl
 LAGS = 6
 
 def LoadMyModel():
 
     season_length = 24 # Define season length as 24 hours for hourly data
-    horizon = LAGS # Forecast horizon is set to 6 hours
+    horizon = LAGS # How many hours will it predict?
     models = [
         AutoARIMA(season_length=season_length), # ARIMA model with automatic order selection and seasonal component
         AutoETS(season_length=season_length), # ETS model with automatic error, trend, and seasonal component
@@ -19,14 +18,12 @@ def LoadMyModel():
 
     sf = StatsForecast(
         models=models, # models for forecasting
-        freq='h',  # frequency of the data
-        n_jobs=-1  # number of jobs to run in parallel, -1 means using all processors
+        freq='h',  # frequency equals hourly
+        n_jobs=-1  # number of jobs to run in parallel, -1 means using all processors but doesn't work for some reason
     )
 
-    sf = StatsForecast.load(path=PATH)
-    Y_hat_df = sf.predict(h=horizon).reset_index()
-    print("Forecast of the models:")
-    print(Y_hat_df.head(LAGS))
+    sf = StatsForecast.load(path=PATH) # Load the model (your .pkl)
+    Y_hat_df = sf.predict(h=horizon).reset_index() # Predicts 6 lags
 
-    return Y_hat_df
+    return Y_hat_df # Returns  results in pandas dataFrame
 
--- Forecasting_Code/RMSE_Test.py
+++ Forecasting_Code/RMSE_Test.py
@@ -1,23 +1,34 @@
 import LoadMyModel
 import numpy as np
+import pandas as pd
+import LoadMyModel
 
-def rmse(y_true, y_pred):
-    y_true = np.array(y_true)
+# Call LoadMyModel.py for predictions
+Y_hat_df = LoadMyModel.LoadMyModel()
+
+# ENTER THE ACTUAL VALUES:
+y_true = [3.2, 3.6, 4, 2.5, 2.9, 4.3]
+Y_hat_df = Y_hat_df.assign(RealValues=y_true)
+
+# Show prediction and actual values
+print("Forecast of the models:")
+print(Y_hat_df)
+
+
+# Does a RMSE test
+def rmse(y_pred):
+    y_true = np.array(Y_hat_df['RealValues'])
     y_pred = np.array(y_pred)
     return np.sqrt(np.mean((y_true - y_pred) ** 2))
 
-Y_hat_df = LoadMyModel.LoadMyModel()
-
-y_true = [3.2, 3.6, 4, 2.5, 2.9, 4.3]  # actual values
-y_AutoArima = Y_hat_df['AutoARIMA']  # predicted values
-y_AutoETS = Y_hat_df['AutoETS'] # predicted values
+# Predicted values:
+y_AutoArima = Y_hat_df['AutoARIMA']
+y_AutoETS = Y_hat_df['AutoETS']
 y_AutoTheta = Y_hat_df['AutoTheta']
 y_CES = Y_hat_df['CES']
 
-print("RMSE AutoARIMA:", rmse(y_true, y_AutoArima))
-
-print("RMSE AutoETS:", rmse(y_true, y_AutoETS))
-
-print("RMSE AutoTheta:", rmse(y_true, y_AutoTheta))
-
-print("RMSE AutoCES:", rmse(y_true, y_CES))
+print()
+print("RMSE AutoARIMA:", rmse(y_AutoArima))
+print("RMSE AutoETS:", rmse(y_AutoETS))
+print("RMSE AutoTheta:", rmse(y_AutoTheta))
+print("RMSE AutoCES:", rmse(y_CES))
